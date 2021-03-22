a81a17a62068e1abdb04fc2c86b47e9c523a2f1c
Change-37 <202110305@bssm.hs.kr>
2021-03-22 21:39:46 +0900

    mathghost making
    made function

---
+++ mathghostgame/mathgame.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+bool TrueNum = false; 
+void RightNum(int arr[], int num) {
+	int a = 0;
+	for (int i = 0; i < num; i++) {
+		a = num % i;
+		if (a == 0) {
+			if (arr[i] == 0) {
+
+			}
+		}
+	}
+
+}
+
+void GetPoint(int pawn, int num) {
+	pawn = pawn + num;
+}
+
+void MakeArray(int arr[]) {
+	for (int i = 0; i < 50; i++) {
+		arr[i] = i+1;
+	}
+}
+
+int main() {
+
+}
